b1fbab57051ef66f34ed270bd4a0804e755bd5f0
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-07-31 07:30:24 +0800

    Update build-360t7.sh

--- build-360t7.sh
+++ build-360t7.sh
@@ -11,7 +11,7 @@ git checkout v25.12.0
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
 mv "${OP_BUILD_PATH}"/360t7.config "${OP_BUILD_PATH}"/immortalwrt/.config
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.88/g' package/base-files/files/bin/config_generate
 make defconfig
 make download -j8
 make V=s -j$(nproc)
